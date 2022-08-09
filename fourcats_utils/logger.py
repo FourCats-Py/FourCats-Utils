765c33b86619605295e7ece6ac512cf9b6fb5942
shiweidong <weidog_shi@outloo.com>
2022-08-09 19:18:30 +0800

    Commit: feat: Submit code to facilitate the development of changing work environment.

--- fourcats_utils/logger.py
+++ fourcats_utils/logger.py
@@ -1,106 +1,114 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
 
-# TIME ： 2022-07-28
-import datetime
-import contextvars
+# TIME ： 2022-08-08
 import json
+import sys
+from typing import Callable
 
-from loguru import logger
+from loguru import logger as _logger
+from loguru._logger import Logger as _Logger
 
 
-class CurrentFormatter:
+class JsonDispose:
     """"""
 
-    uip = contextvars.ContextVar("Uip", default="")
-    trace_id = contextvars.ContextVar("TraceId", default="")
-
-    def __init__(self, uip: str, trace_id: str):
+    def __init__(self):
         """"""
-        self.uip.set(uip)
-        self.trace_id.set(trace_id)
+        self.__json_callback = None
+        self.logger = _logger.patch(self.__dispose)
+        self.logger.add(
+            sink="./json.log", level="DEBUG", format="{extra[serialized]}",
+            filter=lambda record: record["extra"].get("json", False) is True
+        )
 
-    @classmethod
-    def dict(cls):
-        return dict(uip=cls.uip.get(), trace_id=cls.trace_id.get())
-
-
-class JsonLogger:
-    """"""
-
-    def __new__(cls, source: str = None, source_type: str = None, service_id: str = None, *args, **kwargs):
+    def dispose(self, func):
         """"""
-        if source is None:
-            source = ""
+        self.__json_callback = func
+        return func
 
-        if source_type is None:
-            source_type = ""
-
-        if service_id is None:
-            service_id = ""
-
-        return logger.bind(json=True, source=source, source_type=source_type, service_id=service_id)
-
-
-def structure(data: dict):
-    """"""
-    fields = [
-        "alias", "level", "message", "fileline", "datetime", "state", "source", "source_type", "service_id", "uip",
-        "trace_id"
-    ]
-    result = dict(extra=dict())
-    for key, value in data.items():
-        if key in fields:
-            result[key] = str(value)
-        else:
-            result["extra"][key] = str(value)
-
-    difference = set(fields).difference(set(result.keys()))
-    for key in difference:
-        if key == "state":
-            result["state"] = False
-            continue
-        result[key] = ""
-    return result
-
-
-def formatter_for_json(record: dict):
-    """"""
-    record["extra"].pop("json", "")
-    record["extra"].pop("serialized", "")
-    record["extra"]["message"] = record.get("message", "")
-    record["extra"]["level"] = record.get("level", dict()).name
-    record["extra"]["fileline"] = ":".join([record["name"], record["function"], str(record["line"])])
-    record["extra"]["datetime"] = record.get("time", datetime.datetime.now()).strftime("%Y-%m-%d %H:%M:%S.%f")
-    record["extra"]["serialized"] = json.dumps(structure(data=record["extra"]))
-    return "{extra[serialized]}\n"
-
-
-def formatter_fot_stderr(format: str = None, json_format: str = None):
-    """"""
-    if format is None:
-        format = "{time:YYYY-MM-DD HH:mm:ss.SSS} - {level} | {name}:{function}:{line} - {message}\n"
-
-    if json_format is None:
-        json_format = "{time:YYYY-MM-DD HH:mm:ss.SSS} | {level} | {name}:{function}:{line} - {message} - " \
-                      "{extra[serialized]}\n"
-
-    def formatter(record: dict):
+    def __dispose(self, record: dict) -> None:
         """"""
-        return format
-    return formatter
+        if self.__json_callback is not None:
+            return self.__json_callback(record=record)
+        return self.__json_dispose(record=record)
+
+    def __json_dispose(self, record: dict):
+        """"""
+        record["extra"]["serialized"] = json.dumps(record["extra"])
+        return
+
+
+class StderrDispose:
+    """"""
+
+    def __init__(self, logger=_logger, flag: str = "json"):
+        """"""
+        assert isinstance(logger, (_Logger, JsonDispose)), "The log controller type is incorrect."
+
+        if isinstance(logger, JsonDispose):
+            logger = logger.logger
+
+        logger.remove()
+        self.flag = flag
+        self.__stderr_callback = None
+        logger.add(sink=sys.stderr, colorize=True, level="DEBUG", format=self.__dispose)
+
+    def dispose(self, func: Callable):
+        self.__stderr_callback = func
+        return func
+
+    def __dispose(self, record: dict):
+        """"""
+        if self.__stderr_callback is not None:
+            return self.__stderr_callback(record)
+        return self.__stderr_dispose(record=record)
+
+    def __stderr_dispose(self, record: dict) -> str:
+        """"""
+        stderr_formatter = "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | " \
+                           "<level>{level: <8}</level> | " \
+                           "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - " \
+                           "<level>{message}</level>"
+
+        if record["extra"].get(self.flag, False) is True:
+            stderr_formatter += " - <level>{extra[serialized]}</level>"
+
+        if "serialized" not in record["extra"]:
+            record["extra"]["serialized"] = json.dumps(dict())
+
+        stderr_formatter += "\n"
+        return stderr_formatter
+
+
+class Logger:
+    """"""
+
+    def __init__(self):
+        """"""
+        self.json = None
+        self.stderr = StderrDispose(logger=self.json or _logger)
+
+    @property
+    def logger(self):
+        if self.json is None:
+            return _logger
+        return self.json.logger
+
+    def init_app(self):
+        """"""
+        self.logger.add("./app.log", format=formatter_fot_stderr(), level="DEBUG")
+
+    def init_json(self):
+        """"""
+        self.json = JsonDispose()
+        return self.json.logger
 
 
 if __name__ == '__main__':
-    logger.add("./app.log", format=formatter_fot_stderr(), level="DEBUG")
-    logger.add("./json.log", format=formatter_for_json, level="DEBUG",
-               filter=lambda record: record["extra"].get("json", False) is True)
-
-
-
-    CurrentFormatter(uip="1.1.1.1", trace_id="1_1_1_1")
-    json_logger = JsonLogger()
-    json_logger.bind(**CurrentFormatter.dict()).debug("456")
-    logger.debug("123")
-    json_logger.debug("234", json=True)
-    json_logger.bind(**dict(CurrentFormatter.dict())).debug("678")
+    obj = Logger()
+    obj.init_json()
+    obj.logger.debug("1")
+    obj.logger.info("2", json=True)
+    obj.logger.warning("3", json=True)
+    obj.logger.error("4", json=True)
